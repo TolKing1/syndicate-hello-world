0a8ab4f80c4892b94075a3060598bd4bf586eaae
To'lqin <tolking0101@gmail.com>
2024-10-09 22:12:55 +0500

    test 2

--- task02/jsrc/main/java/com/task02/HelloFunctionUrl.java
+++ task02/jsrc/main/java/com/task02/HelloFunctionUrl.java
@@ -5,7 +5,6 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
-import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.task02.handler.RouteHandler;
@@ -15,7 +14,6 @@ import java.util.Map;
 @LambdaHandler(
     lambdaName = "hello_world",
 	roleName = "hello-function-url-role",
-	runtime = DeploymentRuntime.JAVA17,
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
