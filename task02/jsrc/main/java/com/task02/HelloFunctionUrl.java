0a8fa8cf22e21dce1955059f73de51b0e909d7a0
To'lqin <tolking0101@gmail.com>
2024-10-09 20:05:41 +0500

    test 2

--- task02/jsrc/main/java/com/task02/HelloFunctionUrl.java
+++ task02/jsrc/main/java/com/task02/HelloFunctionUrl.java
@@ -3,12 +3,15 @@ package com.task02;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 import com.task02.handler.RouteHandler;
+
+import java.util.Map;
 
 @LambdaHandler(
     lambdaName = "hello_world",
@@ -18,13 +21,16 @@ import com.task02.handler.RouteHandler;
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-@LambdaUrlConfig
-public class HelloFunctionUrl implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
+)
+public class HelloFunctionUrl implements RequestHandler<APIGatewayV2HTTPEvent, Map<String, Object>> {
 	private final RouteHandler routeHandler = new RouteHandler();
 
 
 	@Override
-	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent apiGatewayV2HTTPEvent, Context context) {
+	public Map<String, Object> handleRequest(APIGatewayV2HTTPEvent apiGatewayV2HTTPEvent, Context context) {
 		return routeHandler.handleRequest(apiGatewayV2HTTPEvent);
 	}
 }
--- task02/jsrc/main/java/com/task02/handler/RouteHandler.java
+++ task02/jsrc/main/java/com/task02/handler/RouteHandler.java
@@ -1,38 +1,39 @@
 package com.task02.handler;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.task02.model.RouteKey;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
 public class  RouteHandler {
-    private final Map<RouteKey, Function<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse>> handlers = Map.of(
+    private final Map<RouteKey, Function<APIGatewayV2HTTPEvent, Map<String, Object>>> handlers = Map.of(
             new RouteKey("GET", "/hello"), this::handleGetHello
     );
 
-    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent requestEvent) {
+    public Map<String, Object> handleRequest(APIGatewayV2HTTPEvent requestEvent) {
         RouteKey routeKey = new RouteKey(getMethod(requestEvent), getPath(requestEvent));
         return handlers.getOrDefault(routeKey, this::notFoundResponse).apply(requestEvent);
     }
 
-    private APIGatewayV2HTTPResponse handleGetHello(APIGatewayV2HTTPEvent requestEvent) {
+    private Map<String, Object> handleGetHello(APIGatewayV2HTTPEvent requestEvent) {
         String message = "Hello from lambda";
         return buildResponse(200, message);
     }
 
-    private APIGatewayV2HTTPResponse notFoundResponse(APIGatewayV2HTTPEvent requestEvent) {
+    private Map<String, Object> notFoundResponse(APIGatewayV2HTTPEvent requestEvent) {
         String message = String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s",
                 getPath(requestEvent), getMethod(requestEvent));
         return buildResponse(400, message);
     }
 
-    private APIGatewayV2HTTPResponse buildResponse(int statusCode, String responseBody) {
-        return APIGatewayV2HTTPResponse.builder()
-                .withStatusCode(statusCode)
-                .withBody(responseBody)
-                .build();
+    private Map<String, Object> buildResponse(int statusCode, String responseBody) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("statusCode", statusCode);
+        map.put("message", responseBody);
+
+        return map;
     }
 
     private String getMethod(APIGatewayV2HTTPEvent requestEvent) {
