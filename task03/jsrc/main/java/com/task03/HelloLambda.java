c86c08000392c9311d119ff089f83dca099787c1
To'lqin <tolking0101@gmail.com>
2024-10-16 19:29:13 +0500

    fix: test

--- task03/jsrc/main/java/com/task03/HelloLambda.java
+++ task03/jsrc/main/java/com/task03/HelloLambda.java
@@ -2,11 +2,9 @@ package com.task03;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
-import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.task03.util.ResponseBuilder;
 
 import java.util.Map;
@@ -18,15 +16,11 @@ import java.util.Map;
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-@LambdaUrlConfig(
-		authType = AuthType.NONE
-)
-public class HelloLambda implements RequestHandler<APIGatewayV2HTTPEvent, Map<String, Object>> {
-
+public class HelloLambda implements RequestHandler<APIGatewayProxyRequestEvent, Map<String, Object>> {
 
 	@Override
-	public Map<String, Object> handleRequest(APIGatewayV2HTTPEvent apiGatewayV2HTTPEvent, Context context) {
+	public Map<String, Object> handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
 
-		return ResponseBuilder.buildResponse(200, "{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}");
+		return ResponseBuilder.buildResponse(200, "Hello from Lambda");
 	}
 }
--- task03/jsrc/main/java/com/task03/util/ResponseBuilder.java
+++ task03/jsrc/main/java/com/task03/util/ResponseBuilder.java
@@ -1,13 +1,9 @@
 package com.task03.util;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class ResponseBuilder {
     public static Map<String, Object> buildResponse(int statusCode, String responseBody) {
-        Map<String, Object> resultMap = new HashMap<>();
-        resultMap.put("statusCode", statusCode);
-        resultMap.put("body", responseBody);
-        return resultMap;
+        return Map.of("body", Map.of("statusCode", String.valueOf(statusCode), "message", responseBody));
     }
 }
