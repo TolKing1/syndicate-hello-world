7e02c4871c5387630dfb901e8b51382b99b2a1b9
To'lqin <tolking0101@gmail.com>
2024-10-16 19:35:36 +0500

    fix: auth and api stage

--- task03/jsrc/main/java/com/task03/HelloLambda.java
+++ task03/jsrc/main/java/com/task03/HelloLambda.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.task03.util.ResponseBuilder;
 
@@ -16,6 +17,7 @@ import java.util.Map;
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
+@LambdaUrlConfig
 public class HelloLambda implements RequestHandler<APIGatewayProxyRequestEvent, Map<String, Object>> {
 
 	@Override
