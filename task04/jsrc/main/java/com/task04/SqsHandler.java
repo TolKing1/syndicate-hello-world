071aa0b92a1cdc35a5f68e2ba9a7f026edda22a7
To'lqin <tolking0101@gmail.com>
2024-10-17 17:39:49 +0500

    fix: event source

--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -3,9 +3,8 @@ package com.task04;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.resources.DependsOn;
-import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import java.util.HashMap;
@@ -18,9 +17,9 @@ import java.util.Map;
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-@DependsOn(
-		resourceType = ResourceType.SQS_QUEUE,
-		name = "async_queue"
+@SqsTriggerEventSource(
+		targetQueue = "async_queue",
+		batchSize = 10
 )
 public class SqsHandler implements RequestHandler<SQSEvent , Map<String, Object>> {
 
